3c3273bdf52c02ae75f5ca634c056bcab66583a3
Irene Bertolín <irenebertolin25@gmail.com>
2023-11-12 12:20:45 +0100

    check if file exists + valid data txt

--- scripts/fine_tuning.py
+++ scripts/fine_tuning.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Set your API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def valid_data(data_path):
+def valid_data_jsonl(data_path):
     # Load the dataset
     with open(data_path, 'r', encoding='utf-8') as f:
         dataset = [json.loads(line) for line in f]
@@ -62,8 +62,24 @@ def valid_data(data_path):
         print("No errors found")
         return True
 
+def valid_data_txt(data_path):
+    try:
+        with open(data_path, 'r', encoding='utf-8') as txt_file:
+            for linea in txt_file:
+                if not (linea.startswith('"') and linea.endswith('"')):
+                    return False
+
+    except FileNotFoundError:
+        print(f"The file '{data_path}' was not found.")
+        return False
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+    return True
+
 def upload_file(file_path):
-    # Upload training file
     file_uploaded = openai.File.create(file=open(file_path, "rb"), purpose='fine-tune')
 
     while True:
@@ -77,7 +93,6 @@ def upload_file(file_path):
     return file_uploaded
 
 def create_fine_tuning_job(file_uploaded):
-    # Create fine-tuning job
     fine_tuning_job = openai.FineTuningJob.create(training_file=file_uploaded.id, model="gpt-3.5-turbo")
 
     while True:
@@ -105,30 +120,47 @@ def convert_to_serializable(item):
     else:
         return item
 
-def generate_chat_completion_results(model):
+def create_file(content):
+    existing_files = [item_file for item_file in os.listdir() if item_file.startswith('completion_result') and item_file.endswith('.json')]
+    next_number = 0 if not existing_files else len(existing_files) + 1
+    file_name = f'completion_result{next_number}.json'
+
+    with open("../results/" + file_name, 'w') as result_file:
+        json.dump(content, result_file, indent=4)
+
+    print("Results saved in ../results/" + file_name)
+
+def generate_chat_completion_results(model, dataset_path):
     generated_completion = []
-    with open("../data/test_dataset_content.txt", "r") as test_data:
+    with open(dataset_path, "r") as test_data:
         for line in test_data:
             print('Message: ', line)
             line_obj = {line.strip()}
-            generated_completion.append(line_obj)
-            messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": line}]
+            generated_completion.append('Message: ' + line_obj)
+            messages=[{"role": "system", "content": "Can you tell me what emotion is expressing the message above? It can be one of the following: happiness, sadness, anger, fear, surprise or disgust."}, {"role": "user", "content": line}]
             chat_completion(model, messages, 0.2)
-            generated_completion.append({completion_low_temperature.choices[0].message["content"].strip()})
+            generated_completion.append('Completion with low temperature: ' + {completion_low_temperature.choices[0].message["content"].strip()})
             chat_completion(model, messages, 0.8)
-            generated_completion.append({completion_high_temperature.choices[0].message["content"].strip()})
+            generated_completion.append('Completion with high temperature: ' + {completion_high_temperature.choices[0].message["content"].strip()})
     
     generated_completion = [convert_to_serializable(item) for item in generated_completion]
-    with open("../results/completion_result.json", "w") as result_file:
+    
+    create_file(generated_completion)
+
+    """ with open("../results/completion_result.json", "w") as result_file:
         json.dump(generated_completion, result_file, indent=4)
 
-    print("Results saved in ../results/completion_result.json")
+    print("Results saved in ../results/completion_result.json") """
 
 def main():
     # NOTE: You can change this to any dataset you want to fine-tune on
     training_dataset = "../data/training_dataset.jsonl"
+    # NOTE: You can change this to any dataset you want to test
+    test_dataset = "../data/test_dataset.jsonl"
+    
+    # !: In case of test_dataset = "../data/test_dataset_content.txt", call valid_data_txt(test_dataset)
 
-    if not valid_data(training_dataset):
+    if not valid_data_jsonl(training_dataset) or not valid_data_jsonl(test_dataset):
         return
 
     # Upload training file
@@ -144,7 +176,7 @@ def main():
     # Create chat completion and generate result file
     # model = "ft:gpt-3.5-turbo-0613:universitat-polit-cnica-de-catalunya::8DJxRmih"
     model = fine_tuned_job["fine_tuned_model"]
-    generate_chat_completion_results(model)
+    generate_chat_completion_results(model, test_dataset)
 
 if __name__ == '__main__':
     main()

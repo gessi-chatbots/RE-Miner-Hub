e39fb81923574555fbbf2c184c40c58d75af5a5f
Irene Bertolín <irenebertolin25@gmail.com>
2023-12-30 12:32:45 +0100

    create API for using fine-tuned model

---
+++ OpenAIController.py
@@ -0,0 +1,35 @@
+import flask
+from flask import Flask, request, jsonify
+
+from src.emotion_extraction_handler import EmotionExtractionHandler
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/extract-emotion', methods=['POST'])
+    def get_emotion():
+        try:
+            if 'messages' not in request.json.keys():
+                return "Lacking textual data in proper tag.", 400
+
+            emotion_extraction_handler = EmotionExtractionHandler()
+            data = request.get_json()
+            messages = data.get("messages")
+
+            results = []
+            for message in messages:
+                completion = emotion_extraction_handler.emotion_extraction(message['text'])
+                if completion is None:
+                    return "An error occurred"
+                results.append({'text': message, 'emotion': completion.choices[0].message.content})
+            
+            return jsonify(results)
+
+        except Exception as e:
+            return f"An error occurred: {e}"
+
+    return app
+
+if __name__ == "__main__":
+    flask_app = create_app()
+    flask_app.run(host='0.0.0.0')

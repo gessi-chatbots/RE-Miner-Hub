ac6d1a177df73001c84d4c7530676b8b5b223c86
Irene Bertolín <irenebertolin25@gmail.com>
2024-01-02 18:44:59 +0100

    create connection to NLP-pipeline repo

--- src/API_feature_extraction.py
+++ src/API_feature_extraction.py
@@ -2,17 +2,18 @@ import requests
 
 class APIFeatureExtraction:
     def __init__(self):
-        self.endpoint = 'http://127.0.0.1:5000/extract-features'
+        self.extract_features_endpoint = 'http://127.0.0.1:5000/extract-features'
 
     def extract_features(self, text):
         try:
-            nlp_response = requests.post(self.endpoint, json={'text': text})
+            nlp_response = requests.post(self.extract_features_endpoint, json={'text': text})
 
             if nlp_response.status_code == 200:
                 return nlp_response.json()
             else:
-                return "Error in NLP request", 500
+                raise Exception("Error in NLP request")
+                #return "Error in NLP request", 500
 
         except Exception as e:
-            print("\033[91m\u2718 " + f"An error occurred: {e}")
+            print(f"An error occurred: {e}")
             return None

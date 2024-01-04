9506531a95155aeaab56f7df6dfd17b096769eb8
Irene Bertolín <irenebertolin25@gmail.com>
2024-01-04 16:29:08 +0100

    delete comments

--- src/API_sentiment_analysis.py
+++ src/API_sentiment_analysis.py
@@ -12,14 +12,11 @@ class APISentimentAnalysis:
     def get_emotions(self, model, text):
         try:
             response = requests.post(self.endpoint, params={'tool': model}, headers=self.headers, json={'text': text})
-            #response = requests.post(self.endpoint+"?tool="+model, headers=self.headers, json={'text': text})
-            print("response", response)
 
             if response.status_code == 200:
                 return response.json()
             else:
                 raise Exception("Error in sentiment analysis request")
-                #return "Error in sentiment analysis request", 500
 
         except Exception as e:
             print(f"An error occurred: {e}")

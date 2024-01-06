fe4a50a6edc1fe6f97e4aae1406d801c1cf3c906
Irene Bertolín <irenebertolin25@gmail.com>
2024-01-07 00:11:52 +0100

    create extract_emotion & extract_feature endpoints

--- OpenAIController.py
+++ OpenAIController.py
@@ -1,4 +1,5 @@
 import flask
+import json
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -27,6 +28,83 @@ def create_app():
         }
     )
     app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+    @app.route('/extract-emotion', methods=['POST'])
+    def extract_emotion():
+        try:
+            if 'model' not in request.args.keys():
+                return "Lacking model in proper tag.", 400
+            if 'text' not in request.json.keys():
+                return "Lacking textual data in proper tag.", 400
+            
+            model = request.args.get("model")
+            data = request.get_json()
+            text = data.get("text")
+
+            other_models = ["ParallelDots", "BERT", "BETO", "SVC"]
+
+            results = {}
+
+            if model == "GPT-3.5":
+                emotion_extraction_handler = EmotionExtractionHandler()
+                reviews_with_emotion = emotion_extraction_handler.emotion_extraction(text)
+                for review in reviews_with_emotion:
+                    id_text = review['text']['id']
+                    results[id_text] = {
+                        "emotion": review['emotion'],
+                        "text": review['text']
+                    }
+            elif model in other_models:
+                api_sentiment_analysis = APISentimentAnalysis()
+                for message in text:
+                    emotions = api_sentiment_analysis.get_emotions(model, message['text'])
+                    if emotions is None:
+                        return "Error in sentiment analysis request", 500
+                    else:
+                        results[message['id']] = {
+                            "emotions": emotions['emotions'],
+                            "text": message
+                        }
+            else:
+                return "Model not found", 404
+
+            results = list(results.values())
+
+            return jsonify(results)
+
+        except Exception as e:
+            return f"An error occurred: {e}"
+
+    @app.route('/extract-features', methods=['POST'])
+    def extract_features():
+        try:
+            if 'text' not in request.json.keys():
+                return "Lacking textual data in proper tag.", 400
+            
+            data = request.get_json()
+            text = data.get("text")
+
+            results = {}
+
+            for message in text:
+                results[message['id']] = {
+                    "text": message
+                }
+
+            api_feature_extraction = APIFeatureExtraction()
+            features = api_feature_extraction.extract_features(text)
+
+            for review in features:
+                id_text = review['id']
+                if id_text in results:
+                    results[id_text]['features'] = review['features']
+
+            results = list(results.values())
+
+            return jsonify(results)
+
+        except Exception as e:
+            return f"An error occurred: {e}"
 
     @app.route('/analyze-reviews', methods=['POST'])
     def analyze_reviews():

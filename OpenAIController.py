9c771630f3d7f1bc6c49d1d88fae5a5ba9e84caa
Irene Bertolín <irenebertolin25@gmail.com>
2024-01-10 18:21:17 +0100

    add model inference

--- OpenAIController.py
+++ OpenAIController.py
@@ -3,15 +3,63 @@ import json
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
+from transformers import pipeline
 
 from src.emotion_extraction_handler import EmotionExtractionHandler
 from src.API_feature_extraction import APIFeatureExtraction
 from src.API_sentiment_analysis import APISentimentAnalysis
 
-app = Flask(__name__)
-CORS(app)
+def format_features(text, ner_results):	
+	features = []
+	current_feature = ""
+
+	for feature in ner_results:
+		start = feature['start']
+		end = feature['end']
+
+		# Hot fix to get the whole word
+		blank_space = False
+		while start > 0 and not blank_space and not text[start] == ' ':
+			if text[start-1] != ' ':
+				start -= 1
+			else:
+				blank_space = True
+
+		blank_space = False
+		while end < len(text)-1 and not blank_space and not text[end] == ' ':
+			if text[end+1] != ' ':
+				end += 1
+			else:
+				blank_space = True
+
+		f = text[start:end+1]
+
+		if feature['entity'] == 'B-feature':
+			#If we were processing a feature, it is saved
+			if len(current_feature) > 0:
+				features.append(current_feature.strip().lower())
+				current_feature = ""
+			current_feature += f + " "
+
+		elif feature['entity'] == 'I-feature':
+			# hot fix to make sure a feature does not have the same word twice
+			if f not in current_feature:
+				current_feature += f + " "
+
+		elif feature['entity'] == 'O':
+			#If we were processing a feature, it is saved
+			if len(current_feature) > 0:
+				features.append(current_feature.strip().lower())
+				current_feature = ""
+
+	if len(current_feature) > 0:
+		features.append(current_feature.strip().lower())
+
+	return features
 
 def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
     @app.route('/swagger.yaml')
     def swagger_yaml():
@@ -32,12 +80,14 @@ def create_app():
     @app.route('/extract-emotion', methods=['POST'])
     def extract_emotion():
         try:
-            if 'model' not in request.args.keys():
+            if not request.args or 'model_emotion' not in request.args.keys() and 'text' not in request.json.keys():
+                return "Lacking model and textual data in proper tag.", 400
+            if 'model_emotion' not in request.args.keys():
                 return "Lacking model in proper tag.", 400
             if 'text' not in request.json.keys():
                 return "Lacking textual data in proper tag.", 400
             
-            model = request.args.get("model")
+            model = request.args.get("model_emotion")
             data = request.get_json()
             text = data.get("text")
 
@@ -65,6 +115,8 @@ def create_app():
                             "emotions": emotions['emotions'],
                             "text": message
                         }
+                    max_emotion = max(emotions['emotions'], key=emotions['emotions'].get)
+                    results[message['id']]['emotion'] = max_emotion
             else:
                 return "Model not found", 404
 
@@ -78,12 +130,20 @@ def create_app():
     @app.route('/extract-features', methods=['POST'])
     def extract_features():
         try:
+            if not request.args or 'model_features' not in request.args.keys() and 'text' not in request.json.keys():
+                return "Lacking model and textual data in proper tag.", 400
+            if 'model_features' not in request.args.keys():
+                return "Lacking model in proper tag.", 400
             if 'text' not in request.json.keys():
                 return "Lacking textual data in proper tag.", 400
             
+            model = request.args.get("model_features")
             data = request.get_json()
             text = data.get("text")
 
+            other_models = ["t-frex-bert-base-uncased", "t-frex-bert-large-uncased", "t-frex-roberta-base", "t-frex-roberta-large", "t-frex-xlnet-base-cased", "t-frex-xlnet-large-cased"]
+
+            features_with_id = []
             results = {}
 
             for message in text:
@@ -91,38 +151,52 @@ def create_app():
                     "text": message
                 }
 
-            api_feature_extraction = APIFeatureExtraction()
-            features = api_feature_extraction.extract_features(text)
+            if model == "transfeatex":
+                api_feature_extraction = APIFeatureExtraction()
+                features_with_id = api_feature_extraction.extract_features(text)
+            elif model in other_models:
+                for message in text:
+                    classifier = pipeline("ner", model="quim-motger/"+model)
+                    ner_results = classifier(message['text'])
+                    features = format_features(message['text'], ner_results)
+                    features_with_id.append({'id': message['id'], 'features': features})
+            else:
+                return "Model not found", 404
 
-            for review in features:
+            for review in features_with_id:
                 id_text = review['id']
                 if id_text in results:
-                    results[id_text]['features'] = review['features']
+                    results[id_text]['text']['features'] = review['features']
 
             results = list(results.values())
 
             return jsonify(results)
 
         except Exception as e:
-            return f"An error occurred: {e}"
+            return f"An error occurred: {e}", 400
 
     @app.route('/analyze-reviews', methods=['POST'])
     def analyze_reviews():
         try:
-            if 'model' not in request.args.keys():
+            if not request.args or ('model_emotion' or 'model_features' not in request.args.keys()) and 'text' not in request.json.keys():
+                return "Lacking model and textual data in proper tag.", 400
+            if ('model_emotion' or 'model_features') not in request.args.keys():
                 return "Lacking model in proper tag.", 400
             if 'text' not in request.json.keys():
                 return "Lacking textual data in proper tag.", 400
             
-            model = request.args.get("model")
+            model_emotion = request.args.get("model_emotion")
+            model_features = request.args.get("model_features")
             data = request.get_json()
             text = data.get("text")
 
-            other_models = ["ParallelDots", "BERT", "BETO", "SVC"]
+            other_models_emotion = ["ParallelDots", "BERT", "BETO", "SVC"]
+            other_models_features = ["t-frex-bert-base-uncased", "t-frex-bert-large-uncased", "t-frex-roberta-base", "t-frex-roberta-large", "t-frex-xlnet-base-cased", "t-frex-xlnet-large-cased"]
 
+            features_with_id = []
             results = {}
 
-            if model == "GPT-3.5":
+            if model_emotion == "GPT-3.5":
                 emotion_extraction_handler = EmotionExtractionHandler()
                 reviews_with_emotion = emotion_extraction_handler.emotion_extraction(text)
                 for review in reviews_with_emotion:
@@ -131,10 +205,10 @@ def create_app():
                         "emotion": review['emotion'],
                         "text": review['text']
                     }
-            elif model in other_models:
+            elif model_emotion in other_models_emotion:
                 api_sentiment_analysis = APISentimentAnalysis()
                 for message in text:
-                    emotions = api_sentiment_analysis.get_emotions(model, message['text'])
+                    emotions = api_sentiment_analysis.get_emotions(model_emotion, message['text'])
                     if emotions is None:
                         return "Error in sentiment analysis request", 500
                     else:
@@ -142,13 +216,24 @@ def create_app():
                             "emotions": emotions['emotions'],
                             "text": message
                         }
+                    max_emotion = max(emotions['emotions'], key=emotions['emotions'].get)
+                    results[message['id']]['emotion'] = max_emotion
             else:
                 return "Model not found", 404
 
-            api_feature_extraction = APIFeatureExtraction()
-            features = api_feature_extraction.extract_features(text)
+            if model_features == "transfeatex":
+                api_feature_extraction = APIFeatureExtraction()
+                features_with_id = api_feature_extraction.extract_features(text)
+            elif model_features in other_models_features:
+                for message in text:
+                    classifier = pipeline("ner", model="quim-motger/"+model_features)
+                    ner_results = classifier(message['text'])
+                    features = format_features(message['text'], ner_results)
+                    features_with_id.append({'id': message['id'], 'features': features})
+            else:
+                return "Model not found", 404
 
-            for review in features:
+            for review in features_with_id:
                 id_text = review['id']
                 if id_text in results:
                     results[id_text]['features'] = review['features']
@@ -158,7 +243,7 @@ def create_app():
             return jsonify(results)
 
         except Exception as e:
-            return f"An error occurred: {e}"
+            return f"An error occurred: {e}", 500
 
     return app
 

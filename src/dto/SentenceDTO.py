eb4ba40ba38d60d793073a1f6b1e98cd63e83d31
mtiessler <max.tiessler@upc.edu>
2024-11-11 09:07:21 +0100

    added extraction time to sentences and

--- src/dto/SentenceDTO.py
+++ src/dto/SentenceDTO.py
@@ -1,8 +1,14 @@
 from src.dto.FeatureDTO import FeatureDTO
 from src.dto.SentimentDTO import SentimentDTO
+from datetime import timedelta
 
 class SentenceDTO:
-    def __init__(self, id: str, sentimentData: SentimentDTO, featureData: FeatureDTO, text: str = None):
+    def __init__(self, id: str,
+                 sentimentData: SentimentDTO,
+                 featureData: FeatureDTO,
+                 text: str = None,
+                 extraction_time: timedelta = None):
+        self.extraction_time = extraction_time
         self.id = id
         self.sentimentData = sentimentData
         self.featureData = featureData
@@ -13,5 +19,6 @@ class SentenceDTO:
             "id": self.id,
             "sentimentData": self.sentimentData.to_dict() if self.sentimentData is not None else None,
             "featureData": self.featureData.to_dict() if self.featureData is not None else None,
-            "text": self.text
+            "text": self.text,
+            "extractionTime": self.extraction_time
         }

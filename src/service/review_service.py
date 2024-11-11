0a548987895224dc663579c1be81cb5f5eb0aaa8
mtiessler <max.tiessler@upc.edu>
2024-11-11 08:53:42 +0100

    style

--- src/service/review_service.py
+++ src/service/review_service.py
@@ -3,24 +3,31 @@ from src.dto.SentenceDTO import SentenceDTO
 
 nltk.download('punkt')
 
+
 def add_sentences_to_review(review):
     sentences = split_review(review.review)
     try:
         if len(review.sentences) >= 0:
             for index, sentence in enumerate(sentences):
                 review.sentences[index].text = sentence
-    except IndexError: # TODO check this bug
-            sentence_id = f"{review.reviewId}_{index + 1}"
-            review.sentences.append(SentenceDTO(id=sentence_id, featureData=None, sentimentData=None, text=sentence))
-    
+    except IndexError:  # TODO check this bug
+        sentence_id = f"{review.reviewId}_{index + 1}"
+        review.sentences.append(SentenceDTO(id=sentence_id,
+                                            featureData=None,
+                                            sentimentData=None,
+                                            text=sentence))
+
+
 def is_review_splitted(review):
     return review.sentences is not None and len(review.sentences) > 0
 
+
 def check_review_splitting(review):
     if not is_review_splitted(review):
-        extend_and_split_review(review)  
+        extend_and_split_review(review)
     else:
         add_sentences_to_review(review)
+
 
 def extend_and_split_review(review):
     sentences = split_review(review.review)
@@ -28,6 +35,7 @@ def extend_and_split_review(review):
         sentence_id = f"{review.reviewId}_{index}"
         text = sentence
         review.sentences.append(SentenceDTO(id=sentence_id, featureData=None, sentimentData=None, text=text))
+
 
 def split_review(review_text):
     return nltk.sent_tokenize(review_text)
